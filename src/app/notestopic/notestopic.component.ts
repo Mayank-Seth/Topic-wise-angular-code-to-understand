ecd1b2631b7e303d226b43a2f997f1aa6da31263
Mayank-Seth <sethmayank296@gmail.com>
2022-02-14 11:25:10 +0530

    2 files modified, 3 added

---
+++ src/app/notestopic/notestopic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notestopic',
+  templateUrl: './notestopic.component.html',
+  styleUrls: ['./notestopic.component.css']
+})
+export class NotestopicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
